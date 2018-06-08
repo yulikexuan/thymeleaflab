b2ac7fc7ebc469a384770428a18d7def8ef167bc
yulikexuan <yulikexuan@gmail.com>
2018-06-07 21:48:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Thymeleaf Lab
+
+This repository is for examples of using Thymeleaf with Spring Framework 5
---
+++ src/main/java/com/yulikexuan/thymeleaflab/ThymeleaflabApplication.java
@@ -0,0 +1,12 @@
+package com.yulikexuan.thymeleaflab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaflabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaflabApplication.class, args);
+	}
+}
