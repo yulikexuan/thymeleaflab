2cabff2f20cd8d4ac329a6868cea9d3144a59f1c
yulikexuan <yulikexuan@gmail.com>
2018-06-10 12:46:34 -0400

    Using thymeleaf fragement for page footer

--- src/main/java/com/yulikexuan/thymeleaflab/ThymeleaflabApplication.java
+++ src/main/java/com/yulikexuan/thymeleaflab/ThymeleaflabApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThymeleaflabApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleaflabApplication.class, args);
 	}
