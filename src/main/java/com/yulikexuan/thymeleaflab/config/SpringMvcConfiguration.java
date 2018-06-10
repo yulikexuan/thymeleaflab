4e4d1da6f13e12443edf3a2089688d0f11ffc677
yulikexuan <yulikexuan@gmail.com>
2018-06-10 10:23:53 -0400

    i18n index and product pages using thymeleaf

--- src/main/java/com/yulikexuan/thymeleaflab/config/SpringMvcConfiguration.java
+++ src/main/java/com/yulikexuan/thymeleaflab/config/SpringMvcConfiguration.java
@@ -3,6 +3,40 @@
 
 package com.yulikexuan.thymeleaflab.config;
 
-public class SpringMvcConfiguration {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+
+@Configuration
+public class SpringMvcConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver =
+				new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor =
+				new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 
 }///:~
