9ab08a226e63cefaab5dd6e4ec5f0272ad55382a
yulikexuan <yulikexuan@gmail.com>
2018-06-13 22:02:24 -0400

    Config spring security

--- src/main/java/com/yulikexuan/thymeleaflab/controllers/LoginController.java
+++ src/main/java/com/yulikexuan/thymeleaflab/controllers/LoginController.java
@@ -7,11 +7,7 @@ package com.yulikexuan.thymeleaflab.controllers;
 import com.yulikexuan.thymeleaflab.commands.LoginCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.validation.Valid;
 
 
 @Controller
@@ -23,15 +19,20 @@ public class LoginController {
 		return "loginform";
 	}
 
-	@RequestMapping(value = "/dologin", method = RequestMethod.POST)
-	public String doLogin(@Valid LoginCommand loginCommand,
-	                      BindingResult bindingResult) {
-
-		if (bindingResult.hasErrors()) {
-			return "loginform";
-		}
-
-		return "redirect:/index";
+	@RequestMapping("logout-success")
+	public String yourLoggedOut(){
+		return "logout-success";
 	}
+
+//	@RequestMapping(value = "/dologin", method = RequestMethod.POST)
+//	public String doLogin(@Valid LoginCommand loginCommand,
+//	                      BindingResult bindingResult) {
+//
+//		if (bindingResult.hasErrors()) {
+//			return "loginform";
+//		}
+//
+//		return "redirect:/index";
+//	}
 
 }///:~
